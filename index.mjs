5815ad60c931aff98763943802864905f0929ee2
crccheck <c@crccheck.com>
2022-02-26 11:39:33 -0600

    add /metrics endpoint

--- index.mjs
+++ index.mjs
@@ -1,11 +1,18 @@
 import Koa from 'koa'
 import Router from '@koa/router'
+import promClient from 'prom-client'
 
 const app = new Koa();
 const router = new Router()
 
 router.get('/', (ctx, next) => {
   ctx.body = 'Hello World'
+})
+
+promClient.collectDefaultMetrics()
+router.get('/metrics', async (ctx, next) => {
+  ctx.body = await promClient.register.metrics()
+  ctx.set('Content-Type', promClient.register.contentType)
 })
 
 app.use(router.routes()).use(router.allowedMethods())
