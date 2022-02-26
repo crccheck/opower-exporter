021fdb340f5a0faebaf87cf0b17f7afd1f8a82e6
crccheck <c@crccheck.com>
2022-02-26 11:31:39 -0600

    add koa router

---
+++ index.mjs
@@ -0,0 +1,13 @@
+import Koa from 'koa'
+import Router from '@koa/router'
+
+const app = new Koa();
+const router = new Router()
+
+router.get('/', (ctx, next) => {
+  ctx.body = 'Hello World'
+})
+
+app.use(router.routes()).use(router.allowedMethods())
+
+app.listen(process.env.PORT || 3000);
